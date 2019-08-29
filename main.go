c5187ca332e0a9f76b6d41afae1706da81ece777
poions <877790202@qq.com>
2019-08-29 18:49:24 +0800

    update main:Parameter setting

--- main.go
+++ main.go
@@ -4,13 +4,28 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/common-nighthawk/go-figure"
 	"github.com/poions/GoDomainScan/model"
 )
 
 func main() {
-	fmt.Println(os.Args)
+	figure.NewFigure("GoDomainScan", "", true).Print()
+	fmt.Println("\n")
+	//fmt.Println(os.Args)
 	for i := 0; i < len(os.Args); i++ {
-		//fmt.Println(os.Args[i])
-		model.GetApiRequestResponseData(os.Args[i])
+		switch {
+		case os.Args[i] == "-u":
+			model.GetAPIRequestResponseData(os.Args[i])
+		case os.Args[i] == "-h":
+			fmt.Println("-h/--help \n-u main.go test.com \n-o main.go -u test.com -o /tmp/d.txt")
+		case os.Args[i] == "-o":
+			fmt.Println("test")
+		case os.Args[i] == "--h":
+			fmt.Println("-h/--help \n-u main.go test.com \n-o main.go -u test.com -o /tmp/d.txt")
+		case os.Args[i] == "--help":
+			fmt.Println("-h/--help \n-u main.go test.com \n-o main.go -u test.com -o /tmp/d.txt")
+		case os.Args[i] == "help":
+			fmt.Println("-h/--help \n-u main.go test.com \n-o main.go -u test.com -o /tmp/d.txt")
+		}
 	}
 }
